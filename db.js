5e1727ed0c61d9f78206e1a8e8c4a9a8b25cf867
kazeneza zephilin <kzephilin2@example.com>
2025-11-10 18:55:11 +0200

    Remove hardcoded database credentials, use .env file for security

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString:
-        process.env.DATABASE_URL ||
-        "postgresql://neondb_owner:npg_f1CWvX6uILOj@ep-long-glade-a4updfjp-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
